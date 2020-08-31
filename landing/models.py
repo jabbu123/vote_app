777410617902d34f68270c1dc336a76ef7ab6417
Revanth Tanneeru <tanneeru.revanth7777@gmail.com>
2020-08-31 17:03:08 +0530

    first commit

---
+++ landing/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+        
+
+
+class Question(models.Model): 
+	question_text = models.CharField(max_length = 200) 
+	pub_date = models.DateTimeField('date published') 
+
+	def __str__(self): 
+		return self.question_text 
+
+
+class Choice(models.Model): 
+	question = models.ForeignKey(Question, on_delete = models.CASCADE) 
+	choice_text = models.CharField(max_length = 200) 
+	votes = models.IntegerField(default = 0) 
+
+	def __str__(self): 
+		return self.choice_text 
